73eea71f53c2616b48c99d07b35771005e304f5e
Xu-Angel <1049547382@qq.com>
2020-04-17 17:34:33 +0800

    leran

---
+++ eg/w3schools/q.py
@@ -0,0 +1,26 @@
+# Get the characters from index 2 to index 4 (llo).
+
+txt = "Hello World"
+x = txt[2:5]
+
+
+# eturn the string without any whitespace at the beginning or the end.
+
+
+txt = " Hello World "
+x = txt.strip()
+X = txt.upper()
+txt.lower()
+
+#Insert the correct syntax to add a placeholder for the age parameter.
+
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+# Use the insert method to add "lemon" as the second item in the fruits list.
+
+
+fruits = ["apple", "banana", "cherry"]
+
+fruits.insert(1, "lemon")
